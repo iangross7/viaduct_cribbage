014073a7cc54919a421b870e081784eb10f7f863
Ian Gross <iangross707@gmail.com>
2023-06-12 18:42:42 -0400

    removing jank svg

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion';
-import { ReactComponent as Aos } from '../assets/aceofspades.svg'
 
 import "./Card.css"
 
@@ -13,10 +12,3 @@ export default function Card() {
         </div>
       );
 }
-
-{/* <div className="test-card">
-          <motion.div 
-          drag 
-          dragSnapToOrigin="true"
-          />
-        </div> */}
