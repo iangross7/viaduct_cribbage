e4b42c02fbe07c24d58b9b5c256177d90f0c5727
Ian Gross <iangross707@gmail.com>
2023-06-19 16:18:17 -0400

    finally figured out I had to capitalize components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-
 import Card from "./components/Card.js"
-import { ReactComponent as testCard } from './assets/aceofspades.svg'
 
 function App() {
   return <Card />
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import { motion } from 'framer-motion';
-import {ReactComponent as testCard} from '../assets/aceofspades.svg'
+import { ReactComponent as TestCard} from '../assets/test.svg'
 
 import "./Card.css"
 
 export default function Card() {
     return (
-        <div className='test-card'>
+        <div>
+          <TestCard />
         </div>
       );
 }
