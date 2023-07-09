d89e7b8a50c0966e1d3c4eb2eb295b410ebc0f96
Ian Gross <iangross707@gmail.com>
2023-07-09 11:17:00 -0400

    We now have four cards with acceptable margins. We're so back.

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -7,6 +7,8 @@ export default function Hand() {
         <div className='hand-container'>
             <Card />
             <Card />
+            <Card />
+            <Card />
         </div>
     );
 }
