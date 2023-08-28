e5b9c3c0e77ee89b24fdd3a134b2c5f02e1add33
Ian Gross <iangross707@gmail.com>
2023-08-28 16:45:36 -0500

    Implemented the ability to create static cards, as well.

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -5,12 +5,12 @@ import './Hand.css'
 export default function Hand() {
     return (
         <div className='hand-container'>
-            <MoveableCard cardType='S5'/>
-            <MoveableCard cardType='D5'/>
-            <MoveableCard cardType='C5' />
-            <MoveableCard cardType='H5'/>
-            <MoveableCard cardType='DJ'/>
-            <MoveableCard cardType='SJ'/>
+            <MoveableCard cardType='S5' canMove='true'/>
+            <MoveableCard cardType='D5' canMove='true'/>
+            <MoveableCard cardType='C5' canMove='true'/>
+            <MoveableCard cardType='H5' canMove='true'/>
+            <MoveableCard cardType='DJ' canMove='true'/>
+            <MoveableCard cardType='SJ' canMove='true'/>
         </div>
     );
 }
--- src/components/MoveableCard.js
+++ src/components/MoveableCard.js
@@ -7,11 +7,12 @@ import './MoveableCard.css'
 //TODO: Fix Hitbox
 export default function MoveableCard(props) {
   const cardType = props.cardType;
+  const canMove = props.canMove === 'true';
   const CardSVG = cardSVGs[cardType] || null;
 
   return (
     <div className='card-wrapper'>
-      <motion.div drag dragSnapToOrigin="true">
+      <motion.div drag={canMove} dragSnapToOrigin="true">
         {CardSVG && <CardSVG style={{transform: 'scale(0.5)'}}/>}
       </motion.div>
     </div>
