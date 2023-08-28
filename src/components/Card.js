dba26ef802fd4f9b572e7cf9afb48e5b4057fb0d
Ian Gross <iangross707@gmail.com>
2023-08-28 14:30:05 -0500

    Commenting issues

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,12 @@ import { ReactComponent as TestCard } from '../assets/deckofcards/10c.svg'
 
 import './Card.css'
 
+//TODO: Fix Hitbox
 export default function Card() {
     return (
         <div className='card-wrapper'>
           <motion.div drag dragSnapToOrigin="true">
-            <TestCard style={{transform: 'scale(0.5)'}} /> //TODO: Make Hitbox Smoother
+            <TestCard style={{transform: 'scale(0.5)'}} />
           </motion.div>
         </div>
       );
