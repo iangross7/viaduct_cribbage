4d0578f763d8724a1828f3be12dcdb791c2a9e19
Ian Gross <iangross707@gmail.com>
2023-08-28 21:34:49 -0500

    Creating deck object, made array of IDs

---
+++ src/gameLogic/deck.js
@@ -0,0 +1,62 @@
+const IDS = [
+    'C2',
+    'C3',
+    'C4',
+    'C5',
+    'C6',
+    'C7',
+    'C8',
+    'C9',
+    'CJ',
+    'CQ',
+    'CK',
+    'CA',
+    'S2',
+    'S3',
+    'S4',
+    'S5',
+    'S6',
+    'S7',
+    'S8',
+    'S9',
+    'SJ',
+    'SQ',
+    'SK',
+    'SA',
+    'H2',
+    'H3',
+    'H4',
+    'H5',
+    'H6',
+    'H7',
+    'H8',
+    'H9',
+    'HJ',
+    'HQ',
+    'HK',
+    'HA',
+    'D2',
+    'D3',
+    'D4',
+    'D5',
+    'D6',
+    'D7',
+    'D8',
+    'D9',
+    'DJ',
+    'DQ',
+    'DK',
+    'DA',
+    'C10',
+    'S10',
+    'H10',
+    'D10'
+];
+
+class Card {
+    constructor(id, suit, value) {
+        this.id = id;
+        this.suit = suit;
+        this.value = value;
+    }
+}
