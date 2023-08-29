96d6ce6ebd6b106684cbe50fef2aa8e506529754
Ian Gross <iangross707@gmail.com>
2023-08-29 16:09:03 -0500

    Beginning card removal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import PlayerHand from "./components/PlayerHand.js"
 import AiHand from "./components/AIHand.js";
 import GameState from "./gameLogic/gameState.js";
@@ -8,9 +9,14 @@ import './App.css';
 
 function App() {
   const[gameState, setGameState] = useState(new GameState());
-  console.log(gameState.deck);
-  console.log(gameState.playerHand.cards);
-  console.log(gameState.aiHand.cards);
+  // console.log(gameState.deck);
+  // console.log(gameState.playerHand.cards);
+  // console.log(gameState.aiHand.cards);
+
+  const handleCardPlayedPlayer = (cardID) => {
+    gameState.playerHand.removeCard(cardID);
+    setGameState(gameState);
+  }
 
   return (
     <div className='app-container'>
@@ -18,9 +24,10 @@ function App() {
         <AiHand gameState = {gameState}/>
         <PlayerHand gameState = {gameState}/>
       </div>
-      <div className='play-zone'>
-        
-      </div>
+      <div className='play-zone'></div>
+      <div className='cut-container'></div>
+      <div className='player-score-container'></div>
+      <div className='ai-score-container'></div>
     </div>
   ); 
 }
--- src/gameLogic/hand.js
+++ src/gameLogic/hand.js
@@ -9,8 +9,10 @@ export default class Hand {
         this.numCards++;
     }
 
-    removeCard(card) {
-        var index = this.cards.indexOf(card.id);
+    removeCard(cardID) {
+        console.log(cardID);
+        var index = this.cards.map(e => e.id).indexOf(cardID);
+        console.log(index);
         if (index > -1) {
             this.cards.splice(index, 1);
             this.numCards--;
