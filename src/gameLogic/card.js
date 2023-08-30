8f31051a243ee81bb12c078d3a30628c5d4a19dc
Ian Gross <iangross707@gmail.com>
2023-08-29 23:06:33 -0500

    Added order for cards

--- src/gameLogic/card.js
+++ src/gameLogic/card.js
@@ -1,10 +1,29 @@
 export default class Card {
+    // id is how i've been sorting cards, is C5 is 5 of clubs, suit is suit, symbol is alphanumeric, value is cribbage pegging value
+    // order is in which order the cards come in for runs
     constructor(id, suit, symbol) {
         this.id = id;
         this.suit = suit;
         this.symbol = symbol;
-        if (symbol === "J" || symbol === "Q" || symbol === "K") this.value = 10;
-        else if (symbol ==="A") this.value = 1;
-        else this.value = Number(symbol);
+        if (symbol === "J") {
+            this.value = 10;
+            this.order = 11;
+        }
+        else if (symbol === "Q") {
+            this.value = 10;
+            this.order = 12;
+        }
+        else if (symbol === "K") { 
+            this.value = 10;
+            this.order = 13;
+        }
+        else if (symbol ==="A"){
+            this.value = 1;
+            this.order = 1
+        }
+        else { 
+            this.value = Number(symbol);
+            this.order = Number(symbol);
+        }
     }
 }
---
+++ src/gameLogic/score.js
@@ -0,0 +1,10 @@
+import Hand from "./hand";
+
+class Score {
+    // use for four cards
+    static handScore(hand) {
+
+    }
+
+    static checkForFifteen(handVals)
+}
