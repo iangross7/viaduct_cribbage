f0e52fa5aad06a379f7a32274e4fdac1122b29a0
Ian Gross <iangross707@gmail.com>
2023-09-14 00:46:05 -0500

    Ok actually fixed heart scaling

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -13,8 +13,10 @@ export default function SplashScreen() {
         <>
         {show && (
             <div className="start-screen-container">
-                <h1 className='start-title'>Viaduct Cribbage</h1>
-                <img className='heart' src={heart} alt="Heart Icon" />
+                <div className='start-title-container'>
+                    <h1 className='start-title'>Viaduct Cribbage</h1>
+                    <img className='heart' src={heart} alt="Heart Icon" />
+                </div>
                 <button className='start-button' onClick={startClick}>START GAME</button>
                 <p className='rules-blurb'>Don't know how to play?</p>
                 <p className='source-blurb'>Interested in the project?</p>
