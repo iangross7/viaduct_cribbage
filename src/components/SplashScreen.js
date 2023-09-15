1c5ec4b0c5130b39eab703070f376477189993fe
Ian Gross <iangross707@gmail.com>
2023-09-14 23:40:18 -0500

    Start screen is fully responsive!!

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -13,15 +13,21 @@ export default function SplashScreen() {
         <>
         {show && (
             <div className="start-screen-container">
-                <div className='start-title-container'>
-                    <h1 className='start-title'>Viaduct Cribbage</h1>
-                    <img className='heart' src={heart} alt="Heart Icon" />
+                <div className='start-center-container'>
+                    <div className='start-title-container'>
+                        <h1 className='start-title'>Viaduct Cribbage</h1>
+                        <img className='heart' src={heart} alt="Heart Icon" />
+                    </div>
                     <button className='start-button' onClick={startClick}>START GAME</button>
                 </div>
-                <p className='rules-blurb'>Don't know how to play?</p>
-                <p className='source-blurb'>Interested in the project?</p>
-                <button className='rules-button' onClick={() => window.open('https://bicyclecards.com/how-to-play/cribbage', '_blank')}>GAME RULES</button>
-                <button className='source-button' onClick={() => window.open('https://github.com/iangross7/viaduct_cribbage', '_blank')}>SOURCE CODE</button>
+                <div className='rules-container'>
+                    <p className='rules-blurb'>Don't know how to play?</p>
+                    <button className='rules-button' onClick={() => window.open('https://bicyclecards.com/how-to-play/cribbage', '_blank')}>GAME RULES</button>
+                </div>
+                <div className='source-container'>
+                    <p className='source-blurb'>Interested in the project?</p>
+                    <button className='source-button' onClick={() => window.open('https://github.com/iangross7/viaduct_cribbage', '_blank')}>SOURCE CODE</button>
+                </div>
             </div>
         )}
         </>
