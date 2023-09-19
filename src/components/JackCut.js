f7df221c4d50f3cf809ee4926d7532ee51bd3e6e
Ian Gross <iangross707@gmail.com>
2023-09-19 18:31:50 -0500

    JackCut notification is now responsive

--- src/components/JackCut.js
+++ src/components/JackCut.js
@@ -4,22 +4,22 @@ import './JackCut.css';
 export default function JackCut(props) {
     const gameState = props.gameState;
     let visiblity = 'none';
-    let lcoord = '0%';
+    let lcoord = '77vw';
     if (gameState.cutCard.order === 11 && gameState.currentState === GameState.PEGGING && gameState.humanHand.cards.length === 4) {
         visiblity = 'inline-block';
         if (gameState.humanCrib) {
-            lcoord = '16%';
+            lcoord = '15vw';
         }
         else {
-            lcoord = '77%';
+            lcoord = '77vw';
         }
     }
 
 
     return (
         <div className="jack-cut-container" style={{display: visiblity, left: lcoord}}>
-            <p style={{fontSize: '250%', marginTop: '8%', marginBottom: '6%'}}>+2</p>
-            <h2 style={{fontSize: "100%", marginTop: '0%'}}>FOR JACK CUT!</h2>
+            <p style={{fontSize: '3vw', marginTop: '0%', marginBottom: '6%'}}>+2</p>
+            <h2 style={{fontSize: "1.5vw", marginTop: '0%', marginBottom: '9%'}}>FOR JACK CUT!</h2>
         </div>
     );
 }
