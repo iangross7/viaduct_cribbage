b3d0ef3cb3d6b9f67b7f66abcf926b710e622e20
Ian Gross <iangross707@gmail.com>
2023-09-19 00:41:06 -0500

    Pegging count now reactive

--- src/components/PeggingCount.js
+++ src/components/PeggingCount.js
@@ -9,8 +9,8 @@ export default function PeggingCount(props) {
 
     return (
         <div className="count" style={{display: visibility, textAlign: 'center'}}>
-            <h2 style={{marginBottom: '0.5%', fontSize: '175%', marginTop: '10%'}}>Play Count</h2>
-            <p style={{fontSize: '400%', margin: '0.1% 0'}}>{peggingCount}</p>
+            <h2 style={{marginBottom: '0.5%', fontSize: '1.6vw', marginTop: '10%'}}>Play Count</h2>
+            <p style={{fontSize: '4vw', margin: '0.1% 0'}}>{peggingCount}</p>
         </div>
     );
 }
