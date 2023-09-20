76d76cd3ebce1a9942b35d46a7b775477316198f
Ian Gross <iangross707@gmail.com>
2023-09-19 20:09:57 -0500

    Updated UI fonts

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -2,11 +2,18 @@ import './ScoreBoard.css'
 
 export default function ScoreBoard(props) {
     let player = props.player;
+    let score = props.score;
+    let textColor = 'white';
+
+    if (score >= 121) {
+        textColor = 'red';
+    }
+
     player = player.toUpperCase();
     return (
         <div className="score-container">
             <h2 className='score-header'>{player} SCORE</h2>
-            <p className='score-val'>{props.score}</p>
+            <p className='score-val' style={{color: textColor}}>{score}</p>
         </div>
     );
 }
